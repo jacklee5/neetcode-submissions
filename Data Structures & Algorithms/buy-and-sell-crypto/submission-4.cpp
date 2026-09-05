cb41a357efd55d6a9a4902b2bffb240b83077c0c
Jack Lee <jack200336@gmail.com>
2026-09-05 18:18:59 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // buy starts at 0
+        // sell starts 1
+        // if the next number is more than current sell, move sell
+        // if it larger than buy, move buy to that number
+
+        int best = 0;
+        int buy = 0;
+        int sell = 1;
+        while (sell < prices.size()) {
+            int profit = prices[sell] - prices[buy];
+            best = max(best, profit);
+
+            if (prices[sell] < prices[buy]) {
+                buy = sell;
+            }
+            sell++;
+        }
+
+        return best;
+    }
+};
